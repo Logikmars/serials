b629a0cb1dd9884a4ea1dde767121845b8cb913b
logikmars <dima76673@gmail.com>
2025-04-24 19:21:11 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import './Header.scss';
 
-
 const Header: React.FC = () => {
+    const [activeTab, setActiveTab] = useState("Home");
+    const [isLanguageMenuOpen, setIsLanguageMenuOpen] = useState(false);
 
-    const [activeTab, setactiveTab] = useState("Home");
+    // Функция для переключения состояния меню
+    const toggleLanguageMenu = () => {
+        setIsLanguageMenuOpen((prev) => !prev);
+    };
 
     return (
         <div className='Header container'>
@@ -15,29 +19,39 @@ const Header: React.FC = () => {
                 </div>
                 <div className='Header_left_btns'>
                     <p
-                        className={`Header_left_text ${activeTab === 'Home' ? 'Header_left_text_active' : ''
-                            }`}
+                        className={`Header_left_text ${activeTab === 'Home' ? 'Header_left_text_active' : ''}`}
+                        onClick={() => setActiveTab('Home')}
                     >
                         Home
                     </p>
                     <p
-                        className={`Header_left_text ${activeTab === 'Fandom' ? 'Header_left_text_active' : ''
-                            }`}
+                        className={`Header_left_text ${activeTab === 'Fandom' ? 'Header_left_text_active' : ''}`}
+                        onClick={() => setActiveTab('Fandom')}
                     >
                         Fandom
                     </p>
                 </div>
             </div>
+
             <div className='Header_right'>
                 <div className='Header_right_icons'>
                     <img src="/img/header/search.png" alt="" />
                     <img src="/img/header/likes.png" alt="" />
                     <img src="/img/header/clock.png" alt="" />
                 </div>
-                <div className='Header_right_language'>
+                
+                <div className='Header_right_language' onClick={toggleLanguageMenu}>
                     <img src="/img/header/language.png" alt="" />
                     English
+                    <img src="/img/icons/arrow.svg" alt="" className='Header_right_language_arrow'/>
+                    <ul className={`Header_right_language_dropdown ${isLanguageMenuOpen ? 'Header_right_language_dropdown_show' : ''}`}>
+                        <li>English</li>
+                        <li>Arabic</li>
+                        <li>Tatar</li>
+                        <li>German</li>
+                    </ul>
                 </div>
+
                 <div className='Header_right_signin'>
                     Sign In
                 </div>
@@ -46,6 +60,7 @@ const Header: React.FC = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 };
-export default Header
+
+export default Header;
